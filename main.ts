2d29793e1a19a68f51bab3b06ca0670c0406b995
You <78723629+ian20040409@users.noreply.github.com>
2024-05-16 09:57:04 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let max = 0
 let random_num = 0
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.clearScreen()
     basic.showString("Reset")
     max = 0
 })
@@ -12,6 +13,7 @@ input.onButtonPressed(Button.A, function () {
 input.onGesture(Gesture.Shake, function () {
     random_num = randint(0, max)
     basic.showNumber(random_num)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     max = max + 10
